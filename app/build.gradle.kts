3bbaf1a9106ab17870ebfa9ac357d90cb7e4f057
mobilutils <john@mobilutils.eu>
2026-04-25 20:30:30 +0200

    MacGiver App icon 😁

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "io.github.mobilutils.ntp_dig_ping_more"
         minSdk = 26
         targetSdk { version = release(rootProject.extra["defaultTargetSdkVersion"] as Int) }
-        versionCode = 7
+        versionCode = 8
         versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
