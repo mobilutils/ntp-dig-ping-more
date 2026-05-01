b1c693b42b7381a57a79009ffbd7988a0b488055
mobilutils <john@mobilutils.eu>
2026-05-01 20:38:09 +0200

    updated Relase number to v2.74

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.mobilutils.ntp_dig_ping_more"
         minSdk = 26
         targetSdk { version = release(rootProject.extra["defaultTargetSdkVersion"] as Int) }
-        versionCode = 16
-        versionName = "2.73"
+        versionCode = 17
+        versionName = "2.74"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
