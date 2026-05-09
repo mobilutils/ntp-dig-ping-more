62a0f177777ab22398d7f057d255f28053dd688d
mobilutils <john@mobilutils.eu>
2026-05-09 18:49:11 +0200

    documentation are now generated via nextra v2x

---
+++ docs/next.config.mjs
@@ -0,0 +1,16 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+})
+
+export default withNextra({
+    output: 'export',
+    images: { unoptimized: true },
+
+    // 🎯 Important : basePath correspond au nom de votre repo GitHub
+    // Si votre repo est https://github.com/mobilutils/ntp-dig-ping-more → basePath: '/mon-projet'
+    // Si vous utilisez un domaine personnalisé → laissez ''
+    basePath: process.env.BASE_PATH || '',
+})
