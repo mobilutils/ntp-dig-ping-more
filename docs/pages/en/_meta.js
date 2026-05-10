53fe5b2b03f8f75e7eb7d08f6b51205825b7e2e4
mobilutils <john@mobilutils.eu>
2026-05-10 19:39:17 +0200

    changed organization

--- docs/pages/en/_meta.js
+++ docs/pages/en/_meta.js
@@ -3,6 +3,7 @@ export default {
     'getting-started': 'Getting Started',
     'available-features': 'Available Features',
     'pseudo-commands': 'Pseudo-Commands',
+    'developers': 'Developers',
    github: {
      title: 'GitHub',
      href: 'https://github.com/mobilutils/ntp-dig-ping-more',
---
+++ docs/pages/en/developers/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: 'Intro',
+  'overview': 'Overview',
+  'ntp-client': 'NTP Client',
+  'dig': 'Dig',
+  'ping': 'Ping',
+  'more': '...More',
+}
