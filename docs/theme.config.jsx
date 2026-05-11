02763197d4fbe22ebfbec59b3f7a4c6358df500c
mobilutils <john@mobilutils.eu>
2026-05-11 20:49:34 +0200

    page titles explicit from _meta.js and theme.config.jsx

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -3,4 +3,9 @@ export default {
   logo: <span>🕰️ 🪏 🏓 ⊕... | NTP DIG PING MORE...</span>,
   project: { link: 'https://github.com/mobilutils/ntp-dig-ping-more' },
   logoLink: true,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s | NTP DIG PING MORE',
+    }
+  },
 }
