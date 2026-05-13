fca889afc025066ccdf8226f93af360d251adcc3
mobilutils <john@mobilutils.eu>
2026-05-13 06:47:07 +0200

    a lot of file (re)moved

---
+++ docs/pages/en/bulk-actions/_meta.js
@@ -0,0 +1,6 @@
+// docs/pages/en/bulk-actions/_meta.js
+export default {
+    'index': 'Intro',
+    'bulk-actions-create-use-jsonconfig': 'Using JSON Config.',
+    'bulk-actions-jsonconfig': 'JSON Plenty of Configs',
+}
