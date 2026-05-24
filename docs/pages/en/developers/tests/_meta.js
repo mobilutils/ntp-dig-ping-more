8123a83cedb8beb62e71245a5869fd5b0341e99e
mobilutils <john@mobilutils.eu>
2026-05-25 00:20:39 +0200

    walkthrough on how to setup and push app to workprofile (BYOD use case) Android emulator 9,11,13,15,16 Samsung A34 android 16

--- docs/pages/en/developers/tests/_meta.js
+++ docs/pages/en/developers/tests/_meta.js
@@ -2,4 +2,11 @@
 export default {
   'jacoco-setup': 'JaCoCo Setup',
   'test-managed-configuration-(mdm)': 'Test Managed Config (MDM)',
+  'working-within-workprofile-(mdm)': 'Working within WorkProfile (MDM)',
+  'walkthrough-android-15-workprofile-setup-run': 'Android 15 WorkProfile Walkthrough',
+  'walkthrough-android-16-workprofile-setup-run': 'Android 16 WorkProfile Walkthrough',
+  'walkthrough-android-16-realdevice-samsung-workprofile-setup-run': 'Samsung Android 16 Walkthrough',
+  'walkthrough-android-9-workprofile-setup-run': 'Android 9 WorkProfile Walkthrough',
+  'walkthrough-android-11-workprofile-setup-run': 'Android 11 WorkProfile Walkthrough',
+  'walkthrough-android-13-workprofile-setup-run': 'Android 13 WorkProfile Walkthrough',
 }
